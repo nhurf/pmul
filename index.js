a6109c23e557439f027f5a9009b709aa94fa86f5
nhurf <sergio-nhurf@hotmail.com>
2019-02-04 14:47:50 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,192 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+ 
+io.on('connection', (socket) => {
+
+  socket.on('create', function(room) {
+    socket.join(room);
+  });
+  
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+  });
+ 
+  socket.on('set-nickname', (nickname) => {
+    socket.nickname = nickname;
+    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  });
+
+  socket.on('add-image', (image) => {
+    io.emit('image', {text: image.text, from: socket.nickname, created: new Date()});    
+  });
+
+});
+ 
+var port = process.env.PORT || 3001;
+ 
+http.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
+
+//https://blog.jscrambler.com/mixed-signals-with-socket-io-and-webrtc/ detectar si esta escribiendo
+
+// https://github.com/sivasankars/webrtc-image-snap/blob/master/public/js/main.js
+
+/****************************************************************************
+* WebRTC peer connection and data channel
+****************************************************************************/
+
+var configuration = {
+  'iceServers': [{
+    'urls': 'stun:stun.l.google.com:19302'
+  }]
+};
+
+
+var peerConn;
+var dataChannel;
+
+function signalingMessageCallback(message) {
+  if (message.type === 'offer') {
+    console.log('Got offer. Sending answer to peer.');
+    peerConn.setRemoteDescription(new RTCSessionDescription(message), function() {},
+                                  logError);
+    peerConn.createAnswer(onLocalSessionCreated, logError);
+
+  } else if (message.type === 'answer') {
+    console.log('Got answer.');
+    peerConn.setRemoteDescription(new RTCSessionDescription(message), function() {},
+                                  logError);
+
+  } else if (message.type === 'candidate') {
+    peerConn.addIceCandidate(new RTCIceCandidate({
+      candidate: message.candidate
+    }));
+
+  } else if (message === 'bye') {
+// TODO: cleanup RTC connection?
+}
+}
+
+function createPeerConnection(isInitiator, config) {
+  console.log('Creating Peer connection as initiator?', isInitiator, 'config:',
+              config);
+  peerConn = new RTCPeerConnection(config);
+
+// send any ice candidates to the other peer
+peerConn.onicecandidate = function(event) {
+  console.log('icecandidate event:', event);
+  if (event.candidate) {
+    sendMessage({
+      type: 'candidate',
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate
+    });
+  } else {
+    console.log('End of candidates.');
+  }
+};
+
+if (isInitiator) {
+  console.log('Creating Data Channel');
+  dataChannel = peerConn.createDataChannel('photos');
+  onDataChannelCreated(dataChannel);
+
+  console.log('Creating an offer');
+  peerConn.createOffer(onLocalSessionCreated, logError);
+} else {
+  peerConn.ondatachannel = function(event) {
+    console.log('ondatachannel:', event.channel);
+    dataChannel = event.channel;
+    onDataChannelCreated(dataChannel);
+  };
+}
+}
+
+function onLocalSessionCreated(desc) {
+  console.log('local session created:', desc);
+  peerConn.setLocalDescription(desc, function() {
+    console.log('sending local desc:', peerConn.localDescription);
+    sendMessage(peerConn.localDescription);
+  }, logError);
+}
+
+function onDataChannelCreated(channel) {
+  console.log('onDataChannelCreated:', channel);
+
+  channel.onopen = function() {
+    console.log('CHANNEL opened!!!');
+  };
+
+  channel.onmessage = (adapter.browserDetails.browser === 'firefox') ?
+  receiveDataFirefoxFactory() : receiveDataChromeFactory();
+}
+
+function receiveDataChromeFactory() {
+  var buf, count;
+
+  return function onmessage(event) {
+    if (typeof event.data === 'string') {
+      buf = window.buf = new Uint8ClampedArray(parseInt(event.data));
+      count = 0;
+      console.log('Expecting a total of ' + buf.byteLength + ' bytes');
+      return;
+    }
+
+    var data = new Uint8ClampedArray(event.data);
+    buf.set(data, count);
+
+    count += data.byteLength;
+    console.log('count: ' + count);
+
+    if (count === buf.byteLength) {
+// we're done: all data chunks have been received
+console.log('Done. Rendering photo.');
+renderPhoto(buf);
+}
+};
+}
+
+function receiveDataFirefoxFactory() {
+  var count, total, parts;
+
+  return function onmessage(event) {
+    if (typeof event.data === 'string') {
+      total = parseInt(event.data);
+      parts = [];
+      count = 0;
+      console.log('Expecting a total of ' + total + ' bytes');
+      return;
+    }
+
+    parts.push(event.data);
+    count += event.data.size;
+    console.log('Got ' + event.data.size + ' byte(s), ' + (total - count) +
+                ' to go.');
+
+    if (count === total) {
+      console.log('Assembling payload');
+      var buf = new Uint8ClampedArray(total);
+      var compose = function(i, pos) {
+        var reader = new FileReader();
+        reader.onload = function() {
+          buf.set(new Uint8ClampedArray(this.result), pos);
+          if (i + 1 === parts.length) {
+            console.log('Done. Rendering photo.');
+            renderPhoto(buf);
+          } else {
+            compose(i + 1, pos + this.result.byteLength);
+          }
+        };
+        reader.readAsArrayBuffer(parts[i]);
+      };
+      compose(0, 0);
+    }
+  };
+}
